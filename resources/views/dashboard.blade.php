10c27f7ecc4fe3d55e760bfaf05c5896e97267c0
Harun Doğdu <Harundogdu06@gmail.com>
2021-07-05 17:27:40 +0300

    #2 Jetstream yapılandırıldı Bootstrap kuruldu

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+    <x-slot name="header">
+        {{ __('Anasayfa') }}
+    </x-slot>
+    <div class="alert alert-danger">Selamlar REis</div>
+</x-app-layout>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ $header }}-{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+    @livewireStyles
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+</head>
+
+<body class="font-sans antialiased">
+    <x-jet-banner />
+
+    <div class="min-h-screen bg-gray-100">
+        @livewire('navigation-menu')
+
+        <!-- Page Heading -->
+        @if (isset($header))
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                        {{ $header }}
+                    </h2>
+                </div>
+            </header>
+        @endif
+
+        <div class="py-6">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    {{ $slot }}
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    @stack('modals')
+
+    @livewireScripts
+</body>
+
+</html>
